0e29dc5e88c87b320f11090753c3c54213425f29
Jose Luis Izquierdo Hernandez <joseluisizquierdoshernandez@gmail.com>
2025-02-16 23:22:19 -0600

    Make some changes to the routing and add route to get all the categories product by category

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfullu");
+    console.log("MongoDB connected successfully");
   } catch (error) {
     console.log("MongoDB connection failed");
     process.exit(1);
