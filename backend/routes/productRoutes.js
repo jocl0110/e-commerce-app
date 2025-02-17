626bc555a644203b3ebf40b9f8eb7ac1d4b35d72
Jose Luis Izquierdo Hernandez <joseluisizquierdoshernandez@gmail.com>
2025-02-16 22:51:20 -0600

    Some changes that I forgot to commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,22 @@
 import express from "express";
-
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+} from "../controllers/productController.js";
 const router = express.Router();
 
 // Home Route
 router.get("/home", (req, res) => {
   res.send("Welcome to the Home Page");
 });
+//! GET METHODS
+// Get all products route
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+
+// !POST METHODS
+// Create a new product route
+router.post("/", createProduct);
 
 export default router;
